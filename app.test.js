6e1db1e540580dc62da8fbab00836499bbdf14da
akhilsathyan07 <124149791+akhilsathyan07@users.noreply.github.com>
2024-11-24 12:58:36 +0530

    Delete app.test.js

--- app.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('./app'); // Ensure this path matches your project structure
-
-describe('GET /', () => {
-  it('should return the index page', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
-  });
-});
